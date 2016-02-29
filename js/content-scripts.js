93ce0bfba3bb19b87aa7dca3f78367905deb02ce
Candle5000 <candle5000@icloud.com>
2016-02-29 16:28:15 +0900

    フォームサイズ変更機能を追加

--- js/content-scripts.js
+++ js/content-scripts.js
@@ -14,4 +14,15 @@
 	if(ad_img != null) ad_img.remove();
 	d = document.getElementsByTagName('center');
 	d[d.length-1].remove();
+
+	// 書き込みフォームサイズ変更
+	var subjects = document.getElementsByName('Subject');
+	for(var i = 0; i < subjects.length; i++) {
+		subjects[i].setAttribute('size', '40');
+	}
+	var comments = document.getElementsByName('Comment');
+	for(var i = 0; i < comments.length; i++) {
+		comments[0].cols = 100;
+		comments[0].rows = 24;
+	}
 })(this);
